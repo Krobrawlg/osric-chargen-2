78fd22bebf7a847fcab54e1bb4af64c7ebed4386
Daniel Burt <daniel.lane.burt@gmail.com>
2022-01-26 00:08:47 +0900

    added server connection to mongoDb atlas

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+
+const cors = require("cors");
+
+require("dotenv").config({ path: "./config.env" });
+
+const port = process.env.PORT || 6000;
+
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/items"));
+//get driver connection
+const dbo = require("./db/conn");
+
+app.listen(port, () => {
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+  });
+  console.log(`Server is running on port: ${port}`);
+});
